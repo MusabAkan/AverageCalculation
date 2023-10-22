aea12cd2f33c666ea0cd7dc6672e06fca4e653ba
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-10-22 11:43:06 +0300

    Add project files.

---
+++ Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿namespace AverageCalculation
+{
+    class Fibonacci
+    {
+        public int Depth { get; set; }
+        public int[] Sequence { get; set; }
+
+        public Fibonacci(int depth)
+        {
+            Depth = depth;
+            Sequence = new int[Depth];
+        }
+
+        public void GenerateSequence()
+        {
+            int a = default(int), b = 1;
+            for (int i = default(int); i < Depth; i++)
+            {
+                Sequence[i] = a;
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+
+        public double CalculateAverage()
+        {
+            if (Depth == default(int))
+                return default(int);
+            int sum = default(int);
+            foreach (int number in Sequence)
+                sum += number;
+            return (double)sum / Depth;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using AverageCalculation;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Fibonacci aralığı girin: ");
+            int depth = int.Parse(Console.ReadLine());
+
+            if (depth <= default(int))
+            {
+                Console.WriteLine("Geçersiz aralık değeri. Pozitif bir tamsayı girin.");
+                return;
+            }
+
+            Fibonacci fibonacci = new (depth);
+            fibonacci.GenerateSequence();
+            double average = fibonacci.CalculateAverage();
+
+            Console.WriteLine($"Fibonacci serisi: {string.Join(", ", fibonacci.Sequence)}");
+            Console.WriteLine($"Ortalama: {average:F2}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş. Bir tamsayı girin.");
+        }
+    }
+}
